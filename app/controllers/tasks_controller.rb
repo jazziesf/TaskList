c517fc9a3b787734f6878dcf6d533ccd3149bc67
Jazz <jazziesf21@yahoo.com>
2018-09-18 16:50:55 -0700

    Add two task and updated show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,28 +1,25 @@
-TASKS = [
-  { id: 1, description: "Reverse Sentence Project", complete: "2018, 02, 14"},
-  { id: 2, description:"Join Thursday for a Happy Hour", complete:"2018, 03, 23"},
-  { id: 3, description:"Read Ruby the Hard Way", complete:"2018, 04, 04"},
-  { id: 4, description:"Code-Journal Article", complete:"2018, 08, 21"},
-]
+# TASKS = [
+#   { id: 1, description: "Reverse Sentence Project", complete: "2018, 02, 14"},
+#   { id: 2, description:"Join Thursday for a Happy Hour", complete:"2018, 03, 23"},
+#   { id: 3, description:"Read Ruby the Hard Way", complete:"2018, 04, 04"},
+#   { id: 4, description:"Code-Journal Article", complete:"2018, 08, 21"},
+# ]
 
 
 class TasksController < ApplicationController
 
   def index
-    @tasks = TASKS
+    @tasks = Task.all.order(:name)
   end
 
   def show
     id = params[:id]
-    @task = TASKS.find do |task|
-      task[:id] == id.to_i
-    end
+    @task = Task.find_by(id: id)
 
 
     if id == nil
-      reponse :not_found
+      render :not_found, status: :not_found
     end
-
   end
 
 end
